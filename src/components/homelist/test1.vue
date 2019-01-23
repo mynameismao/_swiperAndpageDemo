099c833cae6160b3d6b1f470083b1b9c4261aabb
manjaroi <703564453@qq.com>
2019-01-23 16:14:36 +0800

    新增页面

---
+++ src/components/homelist/test1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-test1">
+    test1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTest1'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/homelist/test2.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-test2">
+    test2
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTest2'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
